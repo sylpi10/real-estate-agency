67c7c908f99f2bd4f5b38abda3875e6e4a8cf042
sylpi <syl.pillet@hotmail.fr>
2021-02-17 20:21:36 +0100

    add validations

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -6,9 +6,12 @@ use App\Repository\PropertyRepository;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=PropertyRepository::class)
+ * @UniqueEntity("title")
  */
 class Property
 {
@@ -32,6 +35,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min=2, max=120)
      */
     private $title;
 
@@ -42,6 +46,7 @@ class Property
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\Range(min=10, max=500)
      */
     private $surface;
 
@@ -77,6 +82,7 @@ class Property
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Regex("/^[0-9]{5}$/")
      */
     private $postal_code;
 
